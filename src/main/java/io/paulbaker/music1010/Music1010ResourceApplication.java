5fae581343136b1d1db38f4d294fbda7fd020ce0
Paul Baker <paul.baker@overstock.com>
2015-10-13 18:04:23 -0600

    Grabbing port and dumping it to output for easier starting.

--- src/main/java/io/paulbaker/music1010/Music1010ResourceApplication.java
+++ src/main/java/io/paulbaker/music1010/Music1010ResourceApplication.java
@@ -1,9 +1,11 @@
 package io.paulbaker.music1010;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 import java.awt.*;
 import java.net.URI;
@@ -11,8 +13,10 @@ import java.net.URI;
 @SpringBootApplication
 public class Music1010ResourceApplication implements CommandLineRunner {
 
-  @Value("${server.port}")
-  private String currentPort;
+  private Logger logger = Logger.getLogger(this.getClass());
+
+  @Autowired
+  private Environment environment;
 
   public static void main(String[] args) {
     SpringApplication.run(Music1010ResourceApplication.class, args);
@@ -20,11 +24,11 @@ public class Music1010ResourceApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    URI uri = new URI("http://localhost:" + currentPort);
+    URI uri = new URI("http://localhost:" + environment.getProperty("local.server.port"));
     if (Desktop.isDesktopSupported()) {
       Desktop desktop = Desktop.getDesktop();
       desktop.browse(uri);
     }
-    System.out.println(String.format("Point your browser to-> %s", uri));
+    logger.info(String.format("Point your browser to-> %s", uri));
   }
 }
